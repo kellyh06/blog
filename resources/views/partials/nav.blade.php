e2b40b6c232e88792f32cbc7abf52ed81f7715a0
kellyh06 <kelly.hurt@tptlive.ee>
2026-01-10 14:01:31 +0200

    Convert login, register views to DaisyUi

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -2,13 +2,13 @@
     <div class="navbar-start">
         <div class="dropdown">
             <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
+
+                    stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
                 </svg>
             </div>
-            <ul
-                tabindex="-1"
-                class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+            <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li><a>Item 1</a></li>
                 <li>
                     <a>Parent</a>
@@ -28,7 +28,7 @@
             <li>
                 <details>
                     <summary>Parent</summary>
-                    <ul class="p-2 bg-base-100 w-40 z-1">
+                    <ul class="p-2">
                         <li><a>Submenu 1</a></li>
                         <li><a>Submenu 2</a></li>
                     </ul>
@@ -37,7 +37,8 @@
             <li><a>Item 3</a></li>
         </ul>
     </div>
-    <div class="navbar-end">
-        <a class="btn">Button</a>
+    <div class="navbar-end gap-2">
+        <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
+        <a href="{{ route('register') }}" class="btn btn-success">Register</a>
     </div>
 </div>
