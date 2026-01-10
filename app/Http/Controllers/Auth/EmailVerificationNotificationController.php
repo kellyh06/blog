a771a2880ecc72bf054ed7e3db97412afe61f82c
kellyh06 <kelly.hurt@tptlive.ee>
2026-01-10 14:51:42 +0200

    Add auth status to nav and redirect home always

--- app/Http/Controllers/Auth/EmailVerificationNotificationController.php
+++ app/Http/Controllers/Auth/EmailVerificationNotificationController.php
@@ -5,16 +5,23 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-
 class EmailVerificationNotificationController extends Controller
+
 {
+
     /**
+
      * Send a new email verification notification.
+
      */
+
     public function store(Request $request): RedirectResponse
+
     {
+
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false));
+
+            return redirect()->intended(route('home', absolute: false));
         }
 
         $request->user()->sendEmailVerificationNotification();
