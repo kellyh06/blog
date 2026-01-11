a990a9cf99ef2a98903adfc4b47bafd91a75854e
kellyh06 <kelly.hurt@tptlive.ee>
2026-01-11 17:45:21 +0200

    Add likes

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
+
 use App\Models\Post;
 
 use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
 
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -12,20 +16,41 @@ class PublicController extends Controller
 
 {
 
-    public function index() {
+    public function index()
+    {
 
-        $posts = Post::with('user')->withCount('comments')->latest()->simplePaginate(16);
+        $posts = Post::with('user')->withCount('comments', 'likes')->latest()->simplePaginate(16);
 
         return view('welcome', compact('posts'));
-
     }
 
-    public function post(Post $post) {
+    public function post(Post $post)
+    {
 
-        $post->loadCount('comments')->load('comments');
+        $post->loadCount('comments', 'likes')->load('comments');
 
         return view('post', compact('post'));
+    }
 
+    public function like(Post $post)
+    {
+
+        $like = $post->likes()->where('user_id', Auth::user()->id)->first();
+
+        if ($like) {
+
+            $like->delete();
+        } else {
+
+            $like = new Like();
+
+            $like->post()->associate($post);
+
+            $like->user()->associate(Auth::user());
+
+            $like->save();
+        }
+
+        return redirect()->back();
     }
 }
-
